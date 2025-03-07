41423a30eb517de2bd2715566913e5da600856b7
MedEzzedineBenZbiba <mohamedezzedine.benzbiba@esprit.tn>
2025-03-07 04:51:34 +0100

    Home page and sign up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: ["./public/**/*.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'backgroundImg': "url('images/background.png')"
+      },
+
+      fontFamily: {
+        'Montserrat': ["Montserrat", ],
+      },
+      colors: {
+        primary: "#1A2779",
+        secondary: "#1E40AF",
+        ternary_call_to_action : "#1A2779",
+        rose: {
+          50: "#FFF1F2",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
